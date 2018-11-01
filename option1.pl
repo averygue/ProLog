49983462a21cfd75ef42fa45c2d3d7bb36bf646f
Avery-Mac <35672644+averygue@users.noreply.github.com>
2018-11-01 13:42:28 -0500

    Finished Project 1

--- option1.pl
+++ option1.pl
@@ -4,8 +4,8 @@ order([eat(namoi,_),eat(salim,_),eat(edward,_),eat(joan,_)]).
 
 solution(List) :- order(List),
 
-member(eat(naomi,_),List), member(eat(salim,_),List),
-member(eat(edward,_),List), member(eat(joan,_),List),
+member(eat(_,pizza),List), member(eat(_,tacos),List),
+member(eat(_,salad),List), member(eat(_,burgers),List),
 
 
 % Naomi no pizza or burgers
@@ -14,8 +14,8 @@ member(eat(edward,_),List), member(eat(joan,_),List),
 %  Salim no salads
 	(member(eat(salim,pizza),List); member(eat(salim,burgers),List); member(eat(salim,tacos),List)),
 
-%  Edward no tacos
-	(member(eat(edward,pizza),List); member(eat(edward,burgers),List); member(eat(edward,salad),List)),
+%  Edward tacos
+	(member(eat(edward,tacos),List)),
 
 % Joan no pizza
 	(member(eat(joan,tacos),List); member(eat(joan,burgers),List); member(eat(joan,salad),List)).
