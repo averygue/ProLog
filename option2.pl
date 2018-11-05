4cca2df892c95d4633e34a0fd6157028ab0f7c2e
Michael Fuss <30630082+fuss2120@users.noreply.github.com>
2018-11-05 12:48:10 -0600

    Update option2.pl

--- option2.pl
+++ option2.pl
@@ -1,19 +1,67 @@
-% Second Puzzle
-courses([course(_,_,_,_),course(_,_,_,_),course(_,_,_,_),course(_,_,_,_)).
+option2 :-
+	courses(Courses),
 
-solution(List) :- courses(List),
+	member(course(ArtificialIntelligence,_, _, _), Courses),
+	member(course(Compilers,_, _, _), Courses),
+	member(course(OperatingSystems, _, _, _), Courses),
+	member(course(Hardware, _, _, _), Courses),
 
-member(course(hardware,_,_,_),List), member(course(compilers,_,_,_),List),
-member(course(operating_Systems,_,_,_),List), course(Artificial_Intelligence,_,_,_),List),
+	%1. Artificial Intelligence is taught in Room 310.
+	member(course(ArtificialIntelligence, 310, _, _), Courses),
 
-%Artificial_Intelligence @ 9
-(member(course(Artificial_Intelligence,001,Sanders,podium),List); member(course(Artificial_Intelligence,310,Cheng,podium),List)),
+	%2. The Hardware teacher uses a video projector.
+	member(course(Hardware, _, _, videoProjector), Courses),
 
-%operating_Systems @ 10
-(member(course(operating_Systems,001,Sanders,clickers),List)), 
+	% 3. Ms. Saunders teaches in Room 001.
+	member(course(_, 001, Saunders, _), Courses),
 
-%compilers @ 11
-(member(course(compilers,001,Sanders,whiteboard),List); member(course(compilers,126,Adams,whiteboard),List)),
+	% 4. The Compilers class meets in the hour before the class in Room 126.
+	right_of(course(_,126,_,_), course(Compilers,_,_,_), Courses),
 
-%hardware @ 12
-member(course(hardware,126,Adams,projector),List)).
+	% 5. The course in Room 001 uses clickers.
+	member(course(_, 001, _, clickers), Courses),
+
+	% 6. Mr. Shariar uses the whiteboard.
+	member(course(_, _, Shariar, whiteboard), Courses),
+
+	%7. Ms. Cheng teaches the hour before Operating Systems is taught.
+	right_of(course(OperatingSystems,_,_,_), course(_,_,Cheng,_), Courses),
+	%8. Room 112 holds a class at 11:00AM.
+	Courses = [_, _, course(_, 112, _, _), _],
+	%9. Mr. Adams teaches in Room 126.
+	member(course(_, 126, Adams, _), Courses),
+	%10. A podium is used in the class at 9:00AM.
+	Courses = [_, _, _, course(_,_, _,podium)],
+	print_courses(Courses).
+
+% Each course is describe by a structure course with 5 elements
+% representing color, nationality, pet, drink, and cigarette.
+%
+% This courses predicate describes the row of five courses (with
+% all information unknown.
+
+  courses([
+	  course(_, _, _, _),
+	  course(_, _, _, _),
+	  course(_, _, _, _),
+	  course(_, _, _, _),
+	  course(_, _, _, _)
+  ]).
+
+  right_of(A, B, [B, A | _]).
+  right_of(A, B, [_ | Y]) :- right_of(A, B, Y).
+
+  next_to(A, B, [A, B | _]).
+  next_to(A, B, [B, A | _]).
+  next_to(A, B, [_ | Y]) :- next_to(A, B, Y).
+
+  print_courses([A|B]) :- !,
+  	write(A), nl,
+  	print_courses(B).
+  print_courses([]).
+
+%
+% This next line is a directive to automatically run
+% the zebra predicate when this file is consulted.
+%
+:- initialization(option2).
